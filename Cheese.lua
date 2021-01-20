478b5c7afe313963ec989ce4edd166317b3d8194
Advay Patil <advaypatil27@gmail.com>
2021-01-19 22:09:29 -0800

    Added Score and Lives

--- Cheese.lua
+++ Cheese.lua
@@ -1,15 +1,15 @@
-Cookie = Class{}
+Cheese = Class{}
 
-function Cookie:init()
+function Cheese:init()
     self.x = math.random(20, VIRTUAL_WIDTH - 30)
     self.y = math.random(5, 30)
     
-    self.image = love.graphics.newImage('images/cookie.png')
+    self.image = love.graphics.newImage('images/cheese.png')
 
-    self.dy = 50
+    self.dy = 70 
 end
 
-function Cookie:collides(player)
+function Cheese:collides(player)
     if self.x + self.image:getWidth() / 2 > player.x - 14 and self.x + self.image:getWidth() / 2 < player.x + player.img:getWidth() + 14 and self.y < player.y + player.img:getHeight() / 2 and self.y > player.y then
         return true
     end
@@ -17,19 +17,17 @@ function Cookie:collides(player)
     return false
 end
 
-function Cookie:update(dt)
+function Cheese:update(dt)
 
     self.y = self.y + self.dy * dt
 
     if self.y > VIRTUAL_HEIGHT then
         self.y = math.random(5, 30)
         self.x = math.random(20, VIRTUAL_WIDTH - 30)
-        score = math.max(0, score - 1)
+        score = score + 1
     end
 end
 
-
-
-function Cookie:draw()
+function Cheese:draw()
     love.graphics.draw(self.image, math.floor(self.x), math.floor(self.y))
 end
--- main.lua
+++ main.lua
@@ -9,20 +9,31 @@ Class = require 'class'
 
 require 'Player'
 require 'Cookie'
+require 'Cheese'
 
 function love.load()
+
+    score = 0
+    lives = 3
+
+    gameState = 'play'
 
     math.randomseed(os.time())
 
     love.window.setTitle('Cookie Catcher')
 
-    defaultFont = love.graphics.newFont('fonts/font.TTF', 32)
+    defaultFont = love.graphics.newFont('fonts/font.TTF', 16)
 
     player = Player(VIRTUAL_WIDTH / 2 - 30, VIRTUAL_HEIGHT - 30)
+    cheese = {}
     cookies = {}
 
     for i = 0, 3 do
         cookies[i] = Cookie()
+    end
+
+    for i = 0, 1 do
+        cheese[i] = Cheese()
     end
 
     love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -42,17 +53,33 @@ end
 
 function love.update(dt)
     
-    for i = 0, 3 do
-        if cookies[i]:collides(player) then
-            cookies[i].x = math.random(20, VIRTUAL_WIDTH - 30)
-            cookies[i].y = 30
-        end
-
-        cookies[i]:update(dt)
+    if lives == 0 then
+        gameState = 'done'
     end
 
+    if gameState == 'play' then
+        for i = 0, 3 do
+            if cookies[i]:collides(player) then
+                cookies[i].x = math.random(20, VIRTUAL_WIDTH - 30)
+                cookies[i].y = math.random(5, 30)
+                score = score + 1
+            end
 
-    player:update(dt)
+
+            cookies[i]:update(dt)
+        end
+
+        for i = 0, 1 do
+            if cheese[i]:collides(player) then
+                cheese[i].x = math.random(20, VIRTUAL_WIDTH - 30)
+                cheese[i].y = math.random(5, 30)
+                lives = lives - 1
+            end
+
+            cheese[i]:update(dt)
+        end
+        player:update(dt)
+    end
 end
 
 function love.draw()
@@ -60,20 +87,31 @@ function love.draw()
 
     love.graphics.setFont(defaultFont)
 
-    love.graphics.clear(0, 130 / 255, 170 / 255, 1)
+    if gameState == 'play' then
+
+        love.graphics.clear(0, 130 / 255, 170 / 255, 1)
 
     
-    for i = 0, 3 do
-        cookies[i]:draw()
-    end
+        for i = 0, 3 do
+            cookies[i]:draw()
+        end
     
 
 
-    love.graphics.setColor(232 / 255, 105 / 255, 21 / 255, 1)
-    love.graphics.rectangle('fill', 0, VIRTUAL_HEIGHT - 10, VIRTUAL_WIDTH, 10)
-    love.graphics.setColor(1, 1, 1, 1)
+        love.graphics.setColor(232 / 255, 105 / 255, 21 / 255, 1)
+        love.graphics.rectangle('fill', 0, VIRTUAL_HEIGHT - 10, VIRTUAL_WIDTH, 10)
+        love.graphics.setColor(1, 1, 1, 1)
 
-    player:draw()
+        love.graphics.print("Score: " .. tostring(score) .. ". Lives: " .. tostring(lives))
 
+        for i = 0, 1 do
+            cheese[i]:draw()
+        end
+    
+        player:draw()
+    elseif gameState == 'done' then
+        love.graphics.clear(0, 0, 0, 1)
+        love.graphics.print("Game Over", VIRTUAL_WIDTH / 2 - 16, VIRTUAL_HEIGHT / 2 - 16)
+    end 
     push:apply('end')
 end

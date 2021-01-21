3dba8e6f87fcfafce65e06fc7a7dc348c56b6871
Advay Patil <advaypatil27@gmail.com>
2021-01-21 13:17:39 -0800

    Added Winning and some terrible UI

--- main.lua
+++ main.lua
@@ -22,7 +22,7 @@ function love.load()
     lives = 3
 
     -- Initial Game State
-    gameState = 'play'
+    gameState = 'start'
     
     -- Seeds the Random Number generation
     math.randomseed(os.time())
@@ -33,12 +33,17 @@ function love.load()
     -- Defualt Font
     defaultFont = love.graphics.newFont('fonts/font.TTF', 16)
 
+    -- Large Font
+    largeFont = love.graphics.newFont('fonts/font.TTF', 32)
+
     -- Player
     player = Player(VIRTUAL_WIDTH / 2 - 30, VIRTUAL_HEIGHT - 30)
 
     -- Image of the Cookie and Cheese
     cookieIMG = love.graphics.newImage('images/cookie.png')
     cheeseIMG = love.graphics.newImage('images/cheese.png')
+
+    winningIMG = love.graphics.newImage('images/win.png')
 
     -- Cheese and Cookies Tables. Used to store information about 
     -- each cookie and each piece of cheese
@@ -47,6 +52,8 @@ function love.load()
 
     -- Hearts table
     hearts = {}
+
+    normalHeart = love.graphics.newImage('images/normal-heart.png')
 
     -- Populates Hearts Table
     x = 82
@@ -61,11 +68,11 @@ function love.load()
     end
 
     -- Populates cheese table
-    for i = 0, 1 do
+    for i = 0, 3 do
         cheese[i] = Collidable(cheeseIMG, 70)
     end
 
-    -- Sounds tabne to store sounds
+    -- Sounds table to store sounds
     sounds = {
         ['pickup'] = love.audio.newSource('sounds/pickup.wav', 'static'),
         ['lifeLost'] = love.audio.newSource('sounds/hurt.wav', 'static'),
@@ -78,9 +85,13 @@ function love.load()
     -- Sets up the screen
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
-        resizable = false,
+        resizable = true,
         vsync = true
     })
+end
+
+function love.resize(w, h)
+    push:resize(w, h)
 end
 
 function love.keypressed(key)
@@ -90,6 +101,9 @@ function love.keypressed(key)
 
     -- Allows user to restart
     if key == 'enter' or key == 'return' then
+        if gameState == 'start' then
+            gameState = 'play'
+        end
         if gameState == 'done' then
             gameState = 'play'
 
@@ -107,7 +121,31 @@ function love.keypressed(key)
                 cookies[i]:reset()
             end
 
-            for i = 0, 1 do
+            for i = 0, 3 do
+                -- Resets the x and y of the cheese
+                cheese[i]:reset()
+            end
+
+        end
+
+        if gameState == 'win' then
+            gameState = 'play'
+
+            -- Resets values
+            lives = 3
+            score = 0
+
+            for i = 0, 2 do
+                -- Sets the state to the normal state of the heart (red)
+                hearts[i].state = 'norm'
+            end
+
+            for i = 0, 3 do
+                -- Resets the x and y of the cookies
+                cookies[i]:reset()
+            end
+
+            for i = 0, 3 do
                 -- Resets the x and y of the cheese
                 cheese[i]:reset()
             end
@@ -127,7 +165,7 @@ function love.keypressed(key)
                 cookies[i].dy = 0
             end
 
-            for i = 0, 1 do
+            for i = 0, 3 do
                 cheese[i].dy = 0
             end
 
@@ -144,7 +182,7 @@ function love.keypressed(key)
                 end
             end
 
-            for i = 0, 1 do
+            for i = 0, 3 do
                 if score >= 15 then
                     cheese[i].dy = 90
                 else
@@ -159,13 +197,17 @@ function love.keypressed(key)
 end
 
 function love.update(dt)
-    
+
+    if score >= 25 and lives > 0 then
+        gameState = 'win'
+    end
+
     if lives == 0 then
         gameState = 'done'
     end
 
     if score >= 15 then
-        for i = 0, 1 do
+        for i = 0, 3 do
             cheese[i].dy = 110
         end
 
@@ -173,7 +215,7 @@ function love.update(dt)
             cookies[i].dy = 90
         end
     else
-        for i = 0, 1 do
+        for i = 0, 3 do
             cheese[i].dy = 70
         end
 
@@ -183,6 +225,7 @@ function love.update(dt)
     end
 
     if gameState == 'play' then
+
         for i = 0, 3 do
             if cookies[i]:collides(player) then
                 cookies[i]:reset()
@@ -194,7 +237,7 @@ function love.update(dt)
             cookies[i]:update(dt, -1)
         end
 
-        for i = 0, 1 do
+        for i = 0, 3 do
             if cheese[i]:collides(player) then
                 cheese[i]:reset()
                 lives = lives - 1
@@ -213,21 +256,43 @@ function love.draw()
 
     love.graphics.setFont(defaultFont)
 
+    if gameState == 'start' then
+        love.graphics.clear(237 / 255, 175 / 255, 95 / 255, 1)
+
+        love.graphics.setFont(defaultFont)
+
+        love.graphics.setColor(0, 0, 0, 1)
+
+        love.graphics.print("Welcome to Cookie Catcher!", VIRTUAL_WIDTH / 2 - 109, 30)
+
+        love.graphics.print("Press Enter to Start!", VIRTUAL_WIDTH / 2 - 89, VIRTUAL_HEIGHT / 2 - 16)
+    end
+
+    if gameState == 'pause' then
+        love.graphics.print("Paused", VIRTUAL_WIDTH / 2, VIRTUAL_HEIGHT / 2)
+    end
+
     if gameState == 'play' or gameState == 'pause' then
 
         love.graphics.clear(0, 130 / 255, 170 / 255, 1)
 
+        if gameState == 'pause' then
+            love.graphics.setFont(largeFont)
+            love.graphics.print('Paused', 155, 0)
+            love.graphics.setFont(defaultFont)
+        end
     
         for i = 0, 3 do
             cookies[i]:draw()
         end
-    
+        
         player:draw()
+            
 
         love.graphics.setColor(0, 1, 170 / 255, 1)
         love.graphics.print("Score: " .. tostring(score))
 
-        for i = 0, 1 do
+        for i = 0, 3 do
             cheese[i]:draw()
         end
 
@@ -242,11 +307,25 @@ function love.draw()
 
     elseif gameState == 'done' then
         love.graphics.clear(0, 0, 0, 1)
+
         game_over = love.graphics.newImage('images/game-over.png')
+
         love.graphics.draw(game_over, VIRTUAL_WIDTH / 2 - 128, VIRTUAL_HEIGHT / 2 - 128)
+
         love.graphics.setColor(0, 1, 170 / 255, 1)
+        love.graphics.setFont(defaultFont)
+
         love.graphics.print("Final Score: " .. tostring(score), VIRTUAL_WIDTH / 2 - 55)
+
+        love.graphics.print('Press Enter to Restart!', VIRTUAL_WIDTH / 2 - 84, VIRTUAL_HEIGHT - 30)
+
         love.graphics.setColor(1, 1, 1, 1)
+
+    elseif gameState == 'win' then
+        love.graphics.clear(140 / 255, 25 / 255, 1, 1)
+        love.graphics.draw(winningIMG, VIRTUAL_WIDTH / 2 - 64, VIRTUAL_HEIGHT / 2 - 64)
+        love.graphics.setFont(defaultFont)
+        love.graphics.print('Press Enter to Restart!', VIRTUAL_WIDTH / 2 - 84, VIRTUAL_HEIGHT - 30)
     end 
     push:apply('end')
 end
